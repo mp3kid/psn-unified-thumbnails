bbcc9659861f6e27a3b68b5f371ceaceaaaad99b
Tom Brown <08brownt@googlemail.com>
2024-05-25 11:09:08 +0100

    changed js file matching to ensure main menu with query string is still affected, narrowed down element selection to ensure no other elements were accidentally affected, removed title attributes from elements once used to stop the (now redundant) tooltips appearing

--- chrome/script/unified-thumbnails.js
+++ chrome/script/unified-thumbnails.js
@@ -1,17 +1,20 @@
-
-  const stats = document.getElementsByClassName('stat')
+if (window.location.pathname === '/') {
+  
+  const banner = document.getElementById('banner');
+  const stats = banner.getElementsByClassName('stat')
   const uElements = [];
   Array.from(stats).forEach((element) => {
     Array.from(element.getElementsByTagName('u')).forEach((e) => {
       uElements.push(e);
       const value = e.getAttribute('title');
       e.innerHTML = value;
+      e.removeAttribute('title');
     });
   })
-
-  const caps = document.getElementsByClassName('stat-cap');
+  
+  const caps = banner.getElementsByClassName('stat-cap');
   
   while(caps[0]) { //the while condition kinda weird but we chill
     caps[0].parentElement.removeChild(caps[0]);
-  }
-
+  } 
+}
