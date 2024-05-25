1c4cd4e7b7427054a13abc4c933993ec5fd54ead
Tom Brown <08brownt@googlemail.com>
2024-05-25 13:16:42 +0100

    commented out js as the functionality already existed in the site itself lol :duck:

--- chrome/script/unified-thumbnails.js
+++ chrome/script/unified-thumbnails.js
@@ -1,20 +1,20 @@
-if (window.location.pathname === '/') {
+// if (window.location.pathname === '/') {
   
-  const banner = document.getElementById('banner');
-  const stats = banner.getElementsByClassName('stat')
-  const uElements = [];
-  Array.from(stats).forEach((element) => {
-    Array.from(element.getElementsByTagName('u')).forEach((e) => {
-      uElements.push(e);
-      const value = e.getAttribute('title');
-      e.innerHTML = value;
-      e.removeAttribute('title');
-    });
-  })
+//   const banner = document.getElementById('banner');
+//   const stats = banner.getElementsByClassName('stat')
+//   const uElements = [];
+//   Array.from(stats).forEach((element) => {
+//     Array.from(element.getElementsByTagName('u')).forEach((e) => {
+//       uElements.push(e);
+//       const value = e.getAttribute('title');
+//       e.innerHTML = value;
+//       e.removeAttribute('title');
+//     });
+//   })
   
-  const caps = banner.getElementsByClassName('stat-cap');
+//   const caps = banner.getElementsByClassName('stat-cap');
   
-  while(caps[0]) { //the while condition kinda weird but we chill
-    caps[0].parentElement.removeChild(caps[0]);
-  } 
-}
+//   while(caps[0]) { //the while condition kinda weird but we chill
+//     caps[0].parentElement.removeChild(caps[0]);
+//   } 
+// }
