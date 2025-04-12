a7489a94b7bbbeb852d58d17056b805f6e9d4d07
Tom Brown <08brownt@googlemail.com>
2025-04-12 16:47:00 +0100

    cleaned up js, added mutation observer

--- chrome/script/unified-thumbnails.js
+++ chrome/script/unified-thumbnails.js
@@ -1,38 +1,3 @@
-// if (window.location.pathname === '/') {
-
-//   const banner = document.getElementById('banner');
-//   const stats = banner.getElementsByClassName('stat')
-//   const uElements = [];
-//   Array.from(stats).forEach((element) => {
-//     Array.from(element.getElementsByTagName('u')).forEach((e) => {
-//       uElements.push(e);
-//       const value = e.getAttribute('title');
-//       e.innerHTML = value;
-//       e.removeAttribute('title');
-//     });
-//   })
-
-//   const caps = banner.getElementsByClassName('stat-cap');
-
-//   while(caps[0]) { //the while condition kinda weird but we chill
-//     caps[0].parentElement.removeChild(caps[0]);
-//   }
-// }
-
-const rankElements = document.getElementsByClassName("game-rank");
-
-// function getRankByPercentage(percentage) {
-//   if (percentage === null || percentage === undefined) return;
-
-//   if (percentage === 100) return "S";
-//   else if (percentage > 80) return "A";
-//   else if (percentage > 60) return "B";
-//   else if (percentage > 40) return "C";
-//   else if (percentage > 20) return "D";
-//   else if (percentage > 0) return "E";
-//   else return "F";
-// }
-
 function getRankByTrophyNumbers(numTrophies, totalTrophies) {
   if (
     numTrophies === null ||
@@ -54,44 +19,47 @@ function getRankByTrophyNumbers(numTrophies, totalTrophies) {
   else return "F";
 }
 
-for (let i = 0; i < rankElements.length; i++) {
-  const rankElement = rankElements[i];
+function setAllRanks() {
+  console.log("setting ranks......");
+  const MODIFIED_RANK_CLASS = "modified-rank";
+  const rankElements = document.getElementsByClassName("game-rank");
 
-  // console.log("rank element", rankElement);
+  for (let i = 0; i < rankElements.length; i++) {
+    const rankElement = rankElements[i];
 
-  // if (rankElement.tagName !== "span") continue;
+    if (rankElement.classList.contains(MODIFIED_RANK_CLASS)) {
+      continue;
+    }
 
-  const ancestor = rankElement.parentElement?.parentElement?.parentElement;
+    const ancestor = rankElement.parentElement?.parentElement?.parentElement;
+    const smallInfoElement = ancestor.getElementsByClassName("small-info")[0];
+    const smallInfoChildren = smallInfoElement.getElementsByTagName("b");
 
-  // const percentageParent = ancestor.getElementsByClassName("progress-bar")[0];
+    if (smallInfoChildren.length < 2) {
+      continue;
+    }
 
-  // const percentageSpan = percentageParent?.getElementsByTagName("span")[0];
+    const numTrophies = Number.parseInt(smallInfoChildren[0].innerText);
+    const totalTrophies = Number.parseInt(smallInfoChildren[1].innerText);
+    const newRank = getRankByTrophyNumbers(numTrophies, totalTrophies);
 
-  // const percentage = percentageSpan?.innerText?.replace("%", "");
+    if (newRank === undefined) {
+      continue;
+    }
 
-  // const percentageNumber = Number.parseInt(percentage);
-
-  // const newRank = getRankByPercentage(percentageNumber);
-
-  const smallInfoElement = ancestor.getElementsByClassName("small-info")[0];
-
-  const smallInfoChildren = smallInfoElement.getElementsByTagName("b");
-
-  if (smallInfoChildren.length < 2) continue;
-
-  const numTrophies = Number.parseInt(smallInfoChildren[0].innerText);
-
-  const totalTrophies = Number.parseInt(smallInfoChildren[1].innerText);
-
-  const newRank = getRankByTrophyNumbers(numTrophies, totalTrophies);
-
-  // console.log("new rank", newRank);
-
-  if (newRank === undefined) continue;
-
-  rankElement.innerText = newRank;
-
-  rankElement.classList.remove("S", "A", "B", "C", "D", "E");
-
-  rankElement.classList.add(newRank);
+    rankElement.innerText = newRank;
+    rankElement.classList.remove("S", "A", "B", "C", "D", "E");
+    rankElement.classList.add(newRank);
+    rankElement.classList.add(MODIFIED_RANK_CLASS);
+  }
 }
+
+const mutationObserver = new MutationObserver(setAllRanks);
+
+mutationObserver.observe(document, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+});
+
+setAllRanks();
